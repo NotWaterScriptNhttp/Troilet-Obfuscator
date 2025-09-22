6a3e6b470b1f1d698b098c2b5d239b9002b39c5d
PerakZPercic <120364918+PerakZPercic@users.noreply.github.com>
2025-09-22 13:08:32 +0200

    Fixed generics

--- TroiletCore/Plugin/PluginConfigBase.cs
+++ TroiletCore/Plugin/PluginConfigBase.cs
@@ -12,7 +12,7 @@ namespace TroiletCore.Plugin
         protected void AddToggle(string name, bool val = false) => _Settings.Add(new PSettingToggle(name, val));
         protected void AddTextInput(string name, string val = "") => _Settings.Add(new PSettingText(name, val));
         protected void AddRange(string name, int len, int val = 0, int start = 0) => _Settings.Add(new PSettingRange(name, new SRange(start, len), val));
-        protected void AddCombo<T>(string name, T[] options, T? val) => _Settings.Add(new PSettingCombo<T>(name, options, val));
-        protected void AddComboMulti<T>(string name, T[] options, T[]? vals) => _Settings.Add(new PSettingMultiCombo<T>(name, options, vals));
+        protected void AddCombo(string name, object[] options, object? val) => _Settings.Add(new PSettingCombo(name, options, val));
+        protected void AddComboMulti(string name, object[] options, object[]? vals) => _Settings.Add(new PSettingMultiCombo(name, options, vals));
     }
 }
