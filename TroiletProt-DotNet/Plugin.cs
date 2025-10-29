a579980f9710f9c5abc7aa7919a988131b65c9ca
NotWaterScriptNhttp <JustNotKokot.cc.pub.priv@seznam.cz>
2025-10-29 19:53:38 +0100

    Changed some strings

--- TroiletProt-DotNet/Plugin.cs
+++ TroiletProt-DotNet/Plugin.cs
@@ -10,8 +10,8 @@ namespace TroiletProt_DotNet
 {
     public class Plugin : PluginBase, IObfuscatorPlugin
     {
-        public override string Name => ".NET Protector";
-        public override string Description => "Allows troilet to obfuscate .NET assemblies";
+        public override string Name => ".NET Obfuscator";
+        public override string Description => "Troilet's .NET obfuscator";
         public override string Author => "Troilet Team";
         public override Version Version => new Version(1, 0);
         public override PluginConfigBase? Config { get; protected set; } = new PluginConfig();
