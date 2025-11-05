34023add29412cd39128f7bb21309c401dd11972
NotWaterScriptNhttp <JustNotKokot.cc.pub.priv@seznam.cz>
2025-11-05 23:34:05 +0100

    Changed filesize display to kB

--- TroiletGUI/Pages/FilePage.xaml.cs
+++ TroiletGUI/Pages/FilePage.xaml.cs
@@ -100,7 +100,7 @@ namespace TroiletGUI.Pages
             SelectedFile = nfile;
             selectedFile.Text = SelectedFile.Length <= 76 ? SelectedFile : SelectedFile.Substring(0, 38) + "..." + SelectedFile.Substring(SelectedFile.Length - 38, 38);
             fileNameLbl.Content = System.IO.Path.GetFileName(SelectedFile);
-            fileSizeLbl.Content = $"Filesize: 0x{(data == null ? 0 : data.Length):X8}";
+            fileSizeLbl.Content = $"Filesize: {Math.Round(data == null ? 0 : data.Length / 1024f, 2)} kB";
             if (data != null)
                 fileIcon.Source = GetIcon(data, System.IO.Path.GetExtension(nfile));
             else fileIcon.Source = null;
