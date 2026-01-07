dc0acee6310530e95253ddf6dc7dbac054df53da
NotWaterScriptNhttp <JustNotKokot.cc.pub.priv@seznam.cz>
2026-01-08 00:28:09 +0100

    String protection is working now

--- TroiletProt-DotNet/Protections/ConstantProtection.cs
+++ TroiletProt-DotNet/Protections/ConstantProtection.cs
@@ -64,8 +64,57 @@ namespace TroiletProt_DotNet.Protections
             {
                 CilBody body = smeth.Body = new CilBody();
 
-                body.Instructions.Add(OpCodes.Ldarg_0);
-                body.Instructions.Add(OpCodes.Ret);
+                IMethod getChars = module.ImportMethod<string>("get_Chars");
+                IMethod charToString = module.ImportMethod<char>("ToString", new Type[0]);
+                IMethod concat = module.ImportMethod<string>("Concat", new Type[] { typeof(string), typeof(string) });
+                IMethod getLength = module.ImportMethod<string>("get_Length");
+
+                body.Variables.Add(new Local(types.String)); // Result
+                body.Variables.Add(new Local(types.Int32)); // Index
+                body.Variables.Add(new Local(types.Char)); // CurrentChar
+
+                // Set variables to default
+                body.Instructions.Add(OpCodes.Ldstr, ""); // 0
+                body.Instructions.Add(OpCodes.Stloc_0); // 1
+                body.Instructions.Add(OpCodes.Ldc_I4, 0); // 2
+                body.Instructions.Add(OpCodes.Stloc_1); // 3
+
+                // Check length
+                body.Instructions.Add(OpCodes.Br, new InstrIdx(21)); // 4
+
+                // Get current char
+                body.Instructions.Add(OpCodes.Ldarg_0); // 5
+                body.Instructions.Add(OpCodes.Ldloc_1); // 6
+                body.Instructions.Add(OpCodes.Callvirt, getChars); // 7
+
+                // Xor the current char
+                body.Instructions.Add(OpCodes.Ldc_I4, 0x6969); // 8
+                body.Instructions.Add(OpCodes.Xor); // 9
+                body.Instructions.Add(OpCodes.Conv_U2); // 10
+                body.Instructions.Add(OpCodes.Stloc_2); // 11
+
+                // Concat current char with result
+                body.Instructions.Add(OpCodes.Ldloc_0); // 12 str1
+                body.Instructions.Add(OpCodes.Ldloca, new LocalIdx(2)); // 13
+                body.Instructions.Add(OpCodes.Callvirt, charToString); // 14 str2
+                body.Instructions.Add(OpCodes.Call, concat); // 15
+                body.Instructions.Add(OpCodes.Stloc_0); // 16
+
+                // Increment index
+                body.Instructions.Add(OpCodes.Ldloc_1); // 17
+                body.Instructions.Add(OpCodes.Ldc_I4, 1); // 18
+                body.Instructions.Add(OpCodes.Add); // 19
+                body.Instructions.Add(OpCodes.Stloc_1); // 20
+
+                // Check index with length
+                body.Instructions.Add(OpCodes.Ldloc_1); // 21
+                body.Instructions.Add(OpCodes.Ldarg_0); // 22
+                body.Instructions.Add(OpCodes.Callvirt, getLength); // 23
+                body.Instructions.Add(OpCodes.Blt, new InstrIdx(5)); // 24
+
+                // Return result
+                body.Instructions.Add(OpCodes.Ldloc_0); // 25
+                body.Instructions.Add(OpCodes.Ret); // 26
                 body.Instructions.ResolveIndexes(body.Variables);
                 body.Instructions.OptimizeMacros();
                 body.Instructions.OptimizeBranches();
@@ -95,7 +144,7 @@ namespace TroiletProt_DotNet.Protections
                                 case Code.Ldstr:
                                     string str = (string)inst.Operand;
 
-                                    inst.Operand = str;
+                                    inst.Operand = s.ProtectString(str);
                                     body.Instructions.Insert(++i, new Instruction(OpCodes.Call, smeth));
 
                                     s.Val++;
