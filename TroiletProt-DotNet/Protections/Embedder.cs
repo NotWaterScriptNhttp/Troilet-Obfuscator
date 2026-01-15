48bd06de7ea06f3cb985a291305fc2411e0408e7
PerakZPercic <120364918+PerakZPercic@users.noreply.github.com>
2026-01-15 13:06:18 +0100

    Update Embedder.cs

--- TroiletProt-DotNet/Protections/Embedder.cs
+++ TroiletProt-DotNet/Protections/Embedder.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 using dnlib.DotNet;
+using dnlib.DotNet.Emit;
 
 namespace TroiletProt_DotNet.Protections
 {
@@ -12,6 +13,8 @@ namespace TroiletProt_DotNet.Protections
     {
         public class EmbedderSession : ProtectionSession
         {
+            internal TypeDef? Type = null;
+
             public EmbedderSession(ModuleDef module) : base(module) {}
 
             public override ProtectionStatistics EndSession()
@@ -23,6 +26,22 @@ namespace TroiletProt_DotNet.Protections
         public override ProtectionSession StartSession(ModuleDef module)
         {
             EmbedderSession s = new EmbedderSession(module);
+            if (!module.HasResources) // Skip this protection, as there is nothing to protect
+                return s;
+
+            ICorLibTypes types = module.CorLibTypes;
+            s.Type = Globals.CreateType<Embedder>(module);
+
+            MethodBuilder getResB = new MethodBuilder("GetResource", new SZArraySig(types.Byte), new TypeSig[] { types.String, types.UInt64 });
+            // GetResource
+            {
+                getResB.AddInst(OpCodes.Ret);
+            }
+
+            MethodDef getRes = getResB.Get();
+            s.Type.Methods.Add(getRes);
+
+            //TODO: Add logic for finding and replacing the wrapper GetResource
 
             return s;
         }
