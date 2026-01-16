82cad2b5d94e036a5a801057c66ab2d7d7ddaa5f
PerakZPercic <120364918+PerakZPercic@users.noreply.github.com>
2026-01-16 11:35:32 +0100

    Added basic logic

--- TroiletProt-DotNet/Protections/Embedder.cs
+++ TroiletProt-DotNet/Protections/Embedder.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,14 +12,28 @@ namespace TroiletProt_DotNet.Protections
 {
     internal class Embedder : ProtectionBase
     {
+        private struct CompressedEmbedData {
+            public string Name;
+            public byte[] Data;
+        }
         public class EmbedderSession : ProtectionSession
         {
             internal TypeDef? Type = null;
+            internal Dictionary<string, string> _ResNames = new();
 
             public EmbedderSession(ModuleDef module) : base(module) {}
 
+            public string GetResName(string name)
+            {
+                if (_ResNames.TryGetValue(name, out var res))
+                    return res;
+
+                return _ResNames[name] = Globals.GetRandomString();
+            }
+
             public override ProtectionStatistics EndSession()
             {
+                Module.Types.Add(Type);
                 return new ProtectionStatistics();
             }
         }
@@ -32,9 +47,29 @@ namespace TroiletProt_DotNet.Protections
             ICorLibTypes types = module.CorLibTypes;
             s.Type = Globals.CreateType<Embedder>(module);
 
+            using (var ms = new MemoryStream())
+                using (var bw = new BinaryWriter(ms))
+            {
+                EmbeddedResource[] embeds = module.Resources.OfType<EmbeddedResource>().ToArray();
+                foreach (var res in embeds)
+                {
+                    bw.Write(s.GetResName(res.Name));
+                    bw.Write((int)res.Length);
+
+                    var r = res.CreateReader();
+                    bw.Write(r.ReadBytes((int)res.Length));
+
+                    module.Resources.Remove(res);
+                }
+
+                //TODO: Compress data
+                module.Resources.Add(new EmbeddedResource("EmbedderData", ms.ToArray()));
+            }
+
             MethodBuilder getResB = new MethodBuilder("GetResource", new SZArraySig(types.Byte), new TypeSig[] { types.String, types.UInt64 });
             // GetResource
             {
+                getResB.AddInst(OpCodes.Ldnull);
                 getResB.AddInst(OpCodes.Ret);
             }
 
