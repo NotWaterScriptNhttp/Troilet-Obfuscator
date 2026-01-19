ebe647473630ed28534d630d352ae706f5b46a92
NotWaterScriptNhttp <JustNotKokot.cc.pub.priv@seznam.cz>
2026-01-20 00:11:21 +0100

    Update MainWindow.xaml.cs

--- TroiletGUI/MainWindow.xaml.cs
+++ TroiletGUI/MainWindow.xaml.cs
@@ -1,20 +1,15 @@
-﻿using System.Text;
+﻿using System;
+using System.IO;
+using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Controls;
 
 using TroiletCore;
 using TroiletCore.Plugin;
 
 using TroiletGUI.Pages;
 using TroiletGUI.Extensions;
-using System.IO;
 
 namespace TroiletGUI
 {
